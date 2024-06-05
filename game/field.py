2d543c390a24e07f8bc84a6fa1c6cec0afceaa8f
Vladislav Maksimov <workmaximov@yandex.ru>
2024-06-05 15:33:27 +0200

    add checking is rect out of borders

--- game/field.py
+++ game/field.py
@@ -8,7 +8,8 @@ class GameField(QWidget):
         super().__init__()
         self.window_width = constants.WINDOW_WIDTH
         self.window_height = constants.WINDOW_HEIGHT
-        self.setMinimumSize(self.window_width, self.window_height)
+        self.setFixedWidth(self.window_width)
+        self.setFixedHeight(self.window_height)
         self.setWindowTitle('Rectangle game')
 
         self.rectangles = rectangle_collection.RectangleCollection()
@@ -17,12 +18,21 @@ class GameField(QWidget):
         painter = QPainter(self)
         self.rectangles.draw(painter)
 
+    def checkRectOutOfBorders(self, rect: rectangle.Rectangle):
+        if (
+            self.window_width < rect.end.x() 
+            or self.window_height < rect.end.y()
+            or rect.begin.x() < 0
+            or rect.begin.y() < 0
+        ):
+          return True
+        return False  
+
     def mouseDoubleClickEvent(self, event: QMouseEvent | None) -> None:
         x = event.pos().x()
         y = event.pos().y()
         rect = rectangle.Rectangle(x, y)
-        if (self.rectangles.checkCollisions(rect)):
+        if (self.rectangles.checkCollisions(rect) or self.checkRectOutOfBorders(rect)):
             return
         self.rectangles.collection.append(rect)
         self.update()
-        return
